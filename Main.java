72e45399d5f4ac3c51d451d61e76877a255991e0
FEHHJETRAT <121284386+FEHHJETRAT@users.noreply.github.com>
2022-12-25 12:33:32 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 package com.company;
-import java.util.Map;
-import java.util.Map.Entry;
+
 import java.util.Scanner;
-import java.util.Set;
+
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Dictionary dictionary = new Dictionary();
-        Translator translator;
+        Path dictionary = new Path();
+        Trns translator;
         Menu menu = new Menu();
         int solution;
         do {
@@ -20,33 +19,33 @@ public class Main {
             switch (solution) {
 
                 case 1:
-                    System.out.println("Enter english word:");
+                    System.out.println("Введите английское слово:");
                     String english = new Scanner(System.in).nextLine();
-                    System.out.println("Enter translate:");
+                    System.out.println("Введите перевод:");
                     String russia = new Scanner(System.in).nextLine();
                     dictionary.addWord(english, russia);
                     break;
                 case 2:
-                    System.out.println("Enter english sentence:");
+                    System.out.println("Введите предложение на английском:");
                     String englishSentence = new Scanner(System.in).nextLine();
-                    translator = new Translator(dictionary);
+                    translator = new Trns(dictionary);
                     translator.TranslateEnglishToRussia(englishSentence);
                     break;
                 case 3:
-                    System.out.println("Enter russia sentence:");
+                    System.out.println("Введите перевод:");
                     String russiaSentence = new Scanner(System.in).nextLine();
-                    translator = new Translator(dictionary);
+                    translator = new Trns(dictionary);
                     translator.TranslateRussiaToEnglish(russiaSentence);
                     break;
                 case 4:
                     System.out
-                            .println("Enter path to save dictionary(EXAMPLE d:\\java\\dictionary.txt):");
+                            .println("Укажите путь к файлу (например d:\\java\\dictionary.txt):");
                     String path = new Scanner(System.in).nextLine();
                     dictionary.save(path);
                     break;
                 case 5:
                     System.out
-                            .println("Enter path to load dictionary(EXAMPLE d:\\java\\dictionary.txt):");
+                            .println("Укажите путь для сохранения файла(например d:\\java\\dictionary.txt):");
                     path = new Scanner(System.in).nextLine();
                     path.replace("\\", "\\\\");
                     dictionary.load(path);
@@ -54,7 +53,7 @@ public class Main {
                 case 6:
                     if (dictionary.getDictionary().size() == 0)
                         System.out
-                                .println("Your dictionary is empty! Please add word or load dictionary from file.");
+                                .println("База слов пуста! Добавьте новое слово либо загрузите базу из файла.");
                     else System.out.println(dictionary.getDictionary());
                     break;
                 default:
--- Menu.java
+++ Menu.java
@@ -2,14 +2,14 @@ package com.company;
 
 public class Menu {
     public void show() {
-        System.out.println("\n\t\t MENU");
-        System.out.println("Enter: ");
-        System.out.println("1 : Add word to dictionary");
-        System.out.println("2 : Translate from English to Russia");
-        System.out.println("3 : Translate from Russia to English");
-        System.out.println("4 : Save dictionary");
-        System.out.println("5 : Load dictionary");
-        System.out.println("6 : Show dictionary");
-        System.out.println("0 : Exit");
+        System.out.println("\n\t\t Меню");
+        System.out.println("ВВод: ");
+        System.out.println("1 : Добавить слово в базу");
+        System.out.println("2 : ENG - RUS");
+        System.out.println("3 : RUS - ENG");
+        System.out.println("4 : Сохранить базу");
+        System.out.println("5 : Загрузить базу");
+        System.out.println("6 : Показать базу");
+        System.out.println("0 : Выход");
     }
 }
